42b0ea562a9e8dcc5dbfe5e5103cbe1e921ca23d
0761060012 <ntobekovilakazi18@gmail.com>
2023-09-08 17:16:20 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,10 @@
  * main - printing output with printf
  *
  * Return: always (0)
+ *
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0)
+	return (0);
 }
